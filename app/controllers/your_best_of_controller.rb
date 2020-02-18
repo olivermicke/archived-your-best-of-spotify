e37647c828f27b7018ed9c6b78c431f1c375d657
Oliver Micke <olivermicke@outlook.com>
2020-02-18 14:51:10 +0100

    feat: add "genres" section

--- app/controllers/your_best_of_controller.rb
+++ app/controllers/your_best_of_controller.rb
@@ -8,6 +8,28 @@ class YourBestOfController < ApplicationController
     @uses_mocked_data = use_mocked_data? || false
   end
 
+  def genres
+    artists = User.top_artists user_params
+
+    artists_genres = artists.map(&:genres)
+    total_genres = artists_genres.reduce(0) { |n, arr| n + arr.length }
+
+    genres_counted = Hash.new(0)
+    artists_genres.each do |artist|
+      artist.each do |genre|
+        genres_counted[genre] += 1
+      end
+    end
+
+    top_genres = genres_counted.sort_by { |k, v| v }.reverse.take(10).to_h
+    top_genres_with_percentages = top_genres.transform_values { |n| ((n.to_f / total_genres.to_f) * 100).round }
+
+    remaining_percentage = 100 - top_genres_with_percentages.values.sum
+    @genres = (top_genres_with_percentages.merge({ "other" => remaining_percentage })).sort_by { |k, v| v }.reverse.to_h
+    @uses_mocked_data = use_mocked_data? || false
+  end
+
+
   def tracks
     @tracks = User.top_tracks user_params
     @uses_mocked_data = use_mocked_data? || false
