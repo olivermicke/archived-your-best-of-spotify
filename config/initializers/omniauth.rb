8617e519e64820079f223a932629909a89e52bd4
olivermicke <olivermicke@outlook.com>
2022-12-27 17:26:46 +0100

    chore: delete rails application

--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-# typed: false
-# frozen_string_literal: true
-
-require "rspotify/oauth"
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, Rails.application.credentials[:spotify_client_id], Rails.application.credentials[:spotify_client_secret], scope: "user-top-read"
-end
--- lib/tasks/scss.rake
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-namespace :scss do
-  task :lint do
-    sh "scss-lint app/assets/stylesheets"
-  end
-end
