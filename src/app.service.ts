aa894c0f358afcab0ef6594cc8bf998342280c62
Oliver <olivermicke@outlook.com>
2022-12-27 18:09:02 +0100

    feat: add welcome message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! This site is being rebuilt at the moment.';
   }
 }
